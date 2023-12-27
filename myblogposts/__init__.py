14d96a6cf0da0656c0318577fd6772c39cb19761
shubham-ja1n <jainshubham992@gmail.com>
2023-12-27 16:35:26 +0530

    Added database models

--- myblogposts/__init__.py
+++ myblogposts/__init__.py
@@ -1,6 +1,28 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
+
+app.config['secret_key'] = "mysecretkey"
+
+########################  DATABASE SETUP  ###########################
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{basedir}/data.sqlite"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+#####################################################################
+
+#######################  LOGIN CONFIGURATIONS  #####################
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'users.login'
+#####################################################################
+
 
 from myblogposts.core.views import core
 from myblogposts.error_pages.handlers import error_page
